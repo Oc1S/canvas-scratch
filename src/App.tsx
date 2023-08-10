1a47fe5d6e76bccc96da1bdb4776140e2eb16609
Oc1S <ct1147564685@gmail.com>
2023-08-10 22:31:53 +0800

    ✨ feat: fade out

--- src/App.tsx
+++ src/App.tsx
@@ -6,32 +6,48 @@ function App() {
   const [count, setCount] = useState(0)
   const hoverRef = useRef(false)
   const canvasPos = useRef({ x: 0, y: 0 })
-  const touchRef = useRef([{ x: 0, y: 0 }])
-
-  // useEffect(() => {
-  //   const handleMouseDown = () => {
-  //     hoverRef.current = true
-  //   }
-  //   const handleMouseUp = () => {
-  //     hoverRef.current = false
-  //   }
-  //   window.addEventListener('touchstart', handleMouseDown)
-  //   window.addEventListener('touchend', handleMouseUp)
-  //   window.addEventListener('mousedown', handleMouseDown)
-  //   window.addEventListener('mouseup', handleMouseUp)
-  //   return () => {
-  //     window.removeEventListener('touchstart', handleMouseDown)
-  //     window.removeEventListener('touchend', handleMouseUp)
-  //     window.removeEventListener('mousedown', handleMouseDown)
-  //     window.removeEventListener('mouseup', handleMouseUp)
-  //   }
-  // }, [])
+  const fadeRef = useRef(false)
 
   const draw = (ctx: CanvasRenderingContext2D, x: number, y: number) => {
     ctx.globalCompositeOperation = 'destination-out'
     ctx.beginPath()
     ctx.arc(x, y, 10, 0, Math.PI * 2)
     ctx.fill()
+  }
+
+  const fade = (ctx: CanvasRenderingContext2D, alpha: number) => {
+    const fadeOut = () => {
+      ctx.save()
+      ctx.globalCompositeOperation = 'source-in'
+      ctx.fillStyle = ctx.fillStyle + (alpha -= 1).toString(16)
+      ctx.fillRect(0, 0, 400, 300)
+      ctx.restore()
+    }
+
+    // 到210已经看不到涂层了
+    // if (alpha > 210) {
+    //   requestAnimationFrame(() => {
+    //     fade(ctx, alpha)
+    //   })
+    // }
+    const timer = setInterval(() => {
+      if (alpha <= 0) {
+        return clearInterval(timer)
+      }
+      fadeOut()
+    }, 16)
+  }
+
+  const getTransparency = (data: Uint8ClampedArray) => {
+    const { length: total } = data
+    const step = 4
+    let transpentCount = 0
+    for (let i = 3; i < data.length; i += step) {
+      if (data[i] === 0) {
+        transpentCount++
+      }
+    }
+    return transpentCount / (total / step)
   }
 
   useEffect(() => {
@@ -52,28 +68,31 @@ function App() {
       const x = e.offsetX,
         y = e.offsetY
       draw(context, x, y)
+      const { data } = context.getImageData(0, 0, 400, 300)
+      const transparency = getTransparency(data)
+      if (transparency > 0.1) {
+        if (fadeRef.current) return
+        fadeRef.current = true
+        console.log('start')
+        fade(context, 255)
+      }
     }
 
-    const touchDraw = (e: TouchEvent) => {
-      e.preventDefault()
-      console.log(e, e.changedTouches, e.targetTouches, e.touches)
-      const touches = e.targetTouches
-      Array.from(touches).forEach(touchPoint => {
-        const x = touchPoint.pageX,
-          y = touchPoint.pageY
-        draw(context, x - canvasPos.current.x, y - canvasPos.current.y)
-        console.log(canvasPos.current.x, x, x - canvasPos.current.x)
-      })
-      // touches：屏幕上所有触摸点的信息
-      // targetTouches：目标区域上所有触摸点的信息
-      // changedTouches：当前事件触摸点的信息
+    const handleMouseDown = () => {
+      hoverRef.current = true
+    }
+
+    const handleMouseUp = () => {
+      hoverRef.current = false
     }
 
     canvas.addEventListener('mousemove', mouseDraw)
-    canvas.addEventListener('touchmove', touchDraw)
+    window.addEventListener('mousedown', handleMouseDown)
+    window.addEventListener('mouseup', handleMouseUp)
     return () => {
       canvas.removeEventListener('mousemove', mouseDraw)
-      canvas.removeEventListener('touchmove', touchDraw)
+      window.removeEventListener('mousedown', handleMouseDown)
+      window.removeEventListener('mouseup', handleMouseUp)
     }
   }, [])
 
@@ -83,7 +102,9 @@ function App() {
       <canvas id='canvas' width='400' height='300'></canvas>
       <div
         onClick={() => {
+          console.log(count)
           setCount(count + 1)
+          console.log(count)
         }}
       >
         {count}
